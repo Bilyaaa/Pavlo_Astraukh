5b541baa3cff374dddd4d3cd9c99bed4cb8c84b2
Bilyaaa <91008252+Bilyaaa@users.noreply.github.com>
2025-11-12 12:15:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+// let observer = new IntersectionObserver((entries) => {
+
+//     entries.forEach(entry => {
+//        let rect = entry.target.getBoundingClientRect()
+//         console.log(rect.top)
+//     })
+    
+// }, {threshold: 0.5})
+
+
+// observer.observe(document.querySelector('.observe'))
+
+
+
+// document.addEventListener("scroll", () => {
+
+//     document.querySelectorAll(".observe").forEach(e => {
+//         e.classList.remove("active")
+//         let blockTop = e.getBoundingClientRect().top
+//         if (blockTop < window.innerHeight / 3 && blockTop > window.innerHeight / 5) {
+//         console.log(e)
+//         e.classList.add("active")
+//     }
+//     })   
+// })
